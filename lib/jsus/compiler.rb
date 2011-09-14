78efbbb5a2cebb368bcd027c4fac9bc12d61e5d9
Mark Abramov <markizko@gmail.com>
2011-09-14 17:28:40 +0400

    More typos

--- lib/jsus/compiler.rb
+++ lib/jsus/compiler.rb
@@ -18,7 +18,7 @@ module Jsus::Compiler
   end
 
   def generate_includes(package, includes_root, output_file)
-    output_file.open("w") do |f|
+    File.open(output_file, "w") do |f|
       c = Jsus::Container.new(*(@package.source_files.to_a + @package.linked_external_dependencies.to_a))
       paths = c.required_files(includes_root)
       f.puts Jsus::Util::CodeGenerator.generate_includes(paths)
