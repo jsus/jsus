e8e8c0d2d9f78c821284553ce4f09eef93244f4e
Mark Abramov <markizko@gmail.com>
2011-09-15 08:45:52 +0400

    Doc for DirectedAdjacencyGraph#topsorted_vertices

--- lib/extensions/rgl.rb
+++ lib/extensions/rgl.rb
@@ -1,16 +1,25 @@
 module RGL
   class TopsortedGraphHasCycles < Exception; end
   class DirectedAdjacencyGraph
+    # Returns array of topologically sorted vertices. Also checks if there are
+    # cycles.
+    #
+    # @note Default implementation of topsort iterator is a bit faster, but it doesn't
+    # check for that condition.
+    # @return [Array] sorted vertices list
+    # @raise [TopsortedGraphHasCycles] if graph has cycles
+    # @api public
     def topsorted_vertices
-      v_num = vertices.size
       result = []
       available_vertices = []
       reversed_graph = reverse
       out_degrees = {}
+
       vertices.each do |v|
         out_degrees[v] = reversed_graph.out_degree(v)
         available_vertices.push(v) if out_degrees[v] == 0
       end
+
       while available_vertices.size > 0
         vertice = available_vertices.pop
         result.push(vertice)
@@ -20,6 +29,7 @@ module RGL
           available_vertices.push(dependent) if out_degrees[dependent] == 0
         end
       end
+
       raise TopsortedGraphHasCycles unless result.size == vertices.size
       result
     end # topsorted_vertices
