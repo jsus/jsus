4456ea8ad478d6e47ca946223ba483f8e4224a3d
Mark Abramov <markizko@gmail.com>
2011-09-25 21:58:28 +0400

    Focused spec runs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,11 @@ $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), ".."))
 require 'lib/jsus'
 Dir["spec/shared/*.rb"].each {|f| require f}
 
+RSpec.configure do |c|
+  c.filter_run :focus => true
+  c.run_all_when_everything_filtered = true
+end
+
 # cleanup compiled stuff
 def cleanup
   `rm -rf spec/data/Basic/public`
